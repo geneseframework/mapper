77f4c66616c162453a02fcd245ae8f1ab0104e9f
Gilles Fabre <gillesfabre34@gmail.com>
2021-02-12 10:08:46 +0100

    Getting ImportDeclarations of Mapper

--- src/services/flag.service.ts
+++ src/services/flag.service.ts
@@ -1,4 +1,4 @@
-import { MethodDeclaration, Project, SourceFile } from 'ts-morph';
+import { MethodDeclaration, Project, SourceFile, SyntaxKind } from 'ts-morph';
 import * as chalk from 'chalk';
 import { GLOBAL } from '../const/global.const';
 
@@ -7,7 +7,9 @@ export class FlagService {
     static async init(): Promise<void> {
         console.log(chalk.yellowBright('Init mapping...'));
         const initDeclaration: MethodDeclaration = GLOBAL.nodeModuleMapper.getClass('Mapper').getMethod('create');
-        console.log(chalk.blueBright('CREATE DECLARRRRR'), initDeclaration);
+        console.log(chalk.blueBright('CREATE DECLARRRRR'), initDeclaration.getName());
+        const implementations: any = GLOBAL.nodeModuleMapper.getReferencingNodesInOtherSourceFiles().filter(n => n.getKind() === SyntaxKind.ImportDeclaration).map(s => s.getKindName());
+        console.log(chalk.blueBright('IMPLTSSSS'), implementations);
         console.log(chalk.yellowBright('Types mapped'));
     }
 
