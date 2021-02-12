1e88610fa368b11cb3acbae4fa29eab4622deda2
Gilles Fabre <gillesfabre34@gmail.com>
2021-02-12 12:25:44 +0100

    Find sourceFiles importing Mapper

--- src/services/flag.service.ts
+++ src/services/flag.service.ts
@@ -14,14 +14,11 @@ export class FlagService {
 
     static async init(): Promise<void> {
         console.log(chalk.yellowBright('Init mapping...'));
-        const initDeclaration: MethodDeclaration = GLOBAL.nodeModuleMapper.getClass('Mapper').getMethod('create');
         console.log(chalk.greenBright('INIT DECLARRRRR GLOB PROJJJJ'), GLOBAL.project.getSourceFiles().map(s => s.getBaseName()));
-        console.log(chalk.greenBright('INIT DECLARRRRR NODE MOD'), GLOBAL.nodeModuleMapper.getBaseName());
-        console.log(chalk.greenBright('INIT DECLARRRRR REFFFFFS'), GLOBAL.nodeModuleMapper.getReferencingNodesInOtherSourceFiles().map(r => r.getSourceFile().getBaseName()));
-        // console.log(chalk.greenBright('INIT DECLARRRRR'), initDeclaration.getSourceFile().getre());
-        const importDeclarations: ImportDeclaration[] = GLOBAL.nodeModuleMapper.getReferencingNodesInOtherSourceFiles().filter(n => n.getKind() === SyntaxKind.ImportDeclaration) as ImportDeclaration[];
-        // console.log(chalk.blueBright('IMPLTSSSS'), implementations);
-        for (const importDeclaration of importDeclarations) {
+        const referencedNodes: SourceFileReferencingNodes[] = GLOBAL.nodeModuleMapper.getReferencingNodesInOtherSourceFiles();
+        const referencedImportDeclarations: ImportDeclaration[] = referencedNodes.filter(r => GLOBAL.project.getSourceFiles().includes(r.getSourceFile())) as ImportDeclaration[];
+        console.log(chalk.magentaBright('INIT DECLARRRRR REFFFFFS'), referencedImportDeclarations.map(r => r.getSourceFile().getBaseName()));
+        for (const importDeclaration of referencedImportDeclarations) {
             console.log(chalk.blueBright('CREATE DECLARRRRR'), importDeclaration.getSourceFile().getBaseName());
             this.flagTypes(importDeclaration);
         }
