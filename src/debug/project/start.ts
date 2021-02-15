c662888454109f002e302b9d296eb6ce16202ba2
Gilles Fabre <gillesfabre34@gmail.com>
2021-02-15 11:53:28 +0100

    Will fix array of literals

--- src/debug/project/start.ts
+++ src/debug/project/start.ts
@@ -8,21 +8,21 @@ import { Employer } from './src/types/employer.type';
 import { TYPE_EMPLOYER_DATA } from './src/data/type-interface.data';
 import { Ngo } from './src/models/ngo.model';
 import { Person } from './src/models/person.model';
-import { PERSON_LEO } from './src/data/person.data';
+import { PERSON_AUGUSTE, PERSON_LEO } from './src/data/person.data';
 
 async function start() {
     console.log(chalk.yellowBright('Starts application...'));
     // const employer: Employer = await Mapper.create<Employer>('Employer', TYPE_EMPLOYER_DATA, { isType: true });
     // console.log(chalk.yellowBright('Typed object : '), employer);
-    // const person: any = await Mapper.create(Person, PERSON_LEO);
-    // console.log(chalk.yellowBright('Typed object : '), person);
+    const person: any = await Mapper.create(Person, PERSON_AUGUSTE);
+    console.log(chalk.yellowBright('Typed object : '), person);
     // const cats: Cat[] = await Mapper.create(Cat, ARRAY_CAT_DATAS);
     // console.log(chalk.yellowBright('Typed object : '), cats);
     // console.log(chalk.yellowBright('Typed object : '), cats.map(c => c.friend.employer));
     // console.log(chalk.yellowBright('Typed object : '), cats.map(c => c.friend.family.map(f => f.employer)));
     // console.log(chalk.yellowBright('Typed object : '), cats.map(c => c.friend.family.map(f => f.nickNames)));
-    const cat: Cat = await Mapper.create(Cat, CAT_DATA_KITTY);
-    console.log(chalk.yellowBright('Typed object : '), cat);
+    // const cat: Cat = await Mapper.create(Cat, CAT_DATA_KITTY);
+    // console.log(chalk.yellowBright('Typed object : '), cat);
 }
 
 start()
