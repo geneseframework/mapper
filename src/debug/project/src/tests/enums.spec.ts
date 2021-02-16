99adf8df4ab9e1181a14364d10578222f8c04d21
Gilles Fabre <gillesfabre34@gmail.com>
2021-02-16 22:04:07 +0100

    Fixed bug with empty arrays

--- src/debug/project/src/tests/enums.spec.ts
+++ src/debug/project/src/tests/enums.spec.ts
@@ -17,17 +17,19 @@ export class ColorsClassSpec {
 export const testMappers: TestMapper[] = [];
 testMappers.push(new TestMapper(`{ color: 'White' } / ColorClassSpec`, ColorClassSpec, { color: 'White' }));
 testMappers.push(new TestMapper(`{ color: ColorSpec.WHITE } / ColorClassSpec`, ColorClassSpec, { color: ColorSpec.WHITE }));
-testMappers.push(new TestMapper(`{ color: 'Blue' } / ColorClassSpec`, ColorClassSpec, { color: 'Blue' }, { shouldFail: true }));
+testMappers.push(new TestMapper(`{ color: 'Blue' } / ColorClassSpec / {}`, ColorClassSpec, { color: 'Blue' }, { expectedValue: {} }));
 
 
 testMappers.push(new TestMapper(`{ colors: ['White'] } / ColorClassSpec`, ColorsClassSpec, { colors: ['White'] }));
-testMappers.push(new TestMapper(`{ colors: ['Blue'] } / ColorClassSpec / ShouldFail`, ColorsClassSpec, { colors: ['Blue'] }, { shouldFail: true }));
+testMappers.push(new TestMapper(`{ colors: ['Blue'] } / ColorClassSpec / { colors: [] }`, ColorsClassSpec, { colors: [] }));
+testMappers.push(new TestMapper(`{ colors: ['Blue'] } / ColorClassSpec / {}`, ColorsClassSpec, { colors: ['Blue'] }, { expectedValue: {} }));
 testMappers.push(new TestMapper(`{ colors: [ColorSpec.WHITE] } / ColorClassSpec`, ColorsClassSpec, { colors: [ColorSpec.WHITE] }));
 testMappers.push(new TestMapper(`{ colors: [ColorSpec.WHITE, ColorSpec.BLACK] } / ColorClassSpec`, ColorsClassSpec, { colors: [ColorSpec.WHITE, ColorSpec.BLACK] }));
 
 
 testMappers.push(new TestMapper(`'White' / ColorSpec`, 'ColorSpec', 'White'));
-testMappers.push(new TestMapper(`'Blue' / ColorSpec`, 'ColorSpec', 'Blue', { shouldFail: true }));
+testMappers.push(new TestMapper(`'Blue' / ColorSpec / undefined`, 'ColorSpec', 'Blue', { expectedValue: undefined }));
 testMappers.push(new TestMapper(`['White'] / ColorSpec`, 'ColorSpec[]', ['White']));
-testMappers.push(new TestMapper(`['Blue'] / ColorSpec`, 'ColorSpec', ['Blue'], { shouldFail: true }));
+testMappers.push(new TestMapper(`[] / ColorSpec`, 'ColorSpec[]', []));
+testMappers.push(new TestMapper(`['Blue'] / ColorSpec / undefined`, 'ColorSpec', ['Blue'], { expectedValue: undefined }));
 testMappers.push(new TestMapper(`[ColorSpec.WHITE, ColorSpec.BLACK] / ColorSpec`, 'ColorSpec[]', [ColorSpec.WHITE, ColorSpec.BLACK]));
--- src/services/map-array.service.ts
+++ src/services/map-array.service.ts
@@ -6,6 +6,9 @@ import { getNumberOfConstructorArguments } from '../utils/ast-class.util';
 import { TypeDeclaration } from '../types/type-declaration.type';
 import { MapEnumService } from './map-enum.service';
 import { MapInstanceOrInterfaceService } from './map-instance-or-interface.service';
+import * as chalk from 'chalk';
+import { isEnumValue } from '../utils/ast-enums.util';
+import { isEmptyArray } from '../utils/arrays.util';
 
 export class MapArrayService<T> {
 
@@ -13,23 +16,35 @@ export class MapArrayService<T> {
     static map(target: any, key: string, dataValue: any, propertyType: string, apparentType: string): void {
         if (!Array.isArray(dataValue)) {
             return;
+        } else if (isEmptyArray(dataValue)) {
+            target[key] = [];
+            return;
+        } else {
+            this.mapArray(target, key, dataValue, propertyType, apparentType);
         }
+    }
+
+
+    private static mapArray(target: any, key: string, dataValue: any, propertyType: string, apparentType: string): void {
         const typeName: string = propertyType.slice(0, -2);
         const importArrayDeclaration: TypeDeclaration = getImportTypeDeclaration(apparentType, typeName);
-        target[key] = [] as any[];
         for (const element of dataValue) {
             if (importArrayDeclaration instanceof ClassDeclaration) {
                 const instanceGenerator = new InstanceGenerator(typeName, getApparentTypeImportDeclarationPath(apparentType), getNumberOfConstructorArguments(importArrayDeclaration));
                 const instance = GLOBAL.generateInstance(instanceGenerator);
                 MapInstanceOrInterfaceService.map(element, instance, importArrayDeclaration);
-                target[key].push(instance);
+                this.push(target, key, instance);
             }
-            if (importArrayDeclaration instanceof EnumDeclaration) {
-                const root = { rootKey: [] };
-                MapEnumService.map(root, 'rootKey', element, importArrayDeclaration);
-                target[key].push(root.rootKey);
+            if (importArrayDeclaration instanceof EnumDeclaration && isEnumValue(importArrayDeclaration, element)) {
+                this.push(target, key, element);
             }
         }
+    }
+
+
+    private static push(target: any, key: string, element: any): void {
+        target[key] = target[key] ?? [] as any[];
+        target[key].push(element);
     }
 
 }
