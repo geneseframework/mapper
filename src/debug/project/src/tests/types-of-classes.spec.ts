6941d3e7ab6e8e34b7033048df588de021312317
Gilles Fabre <gillesfabre34@gmail.com>
2021-02-16 22:12:22 +0100

    30 tests runs

--- src/debug/project/src/tests/types-of-classes.spec.ts
+++ src/debug/project/src/tests/types-of-classes.spec.ts
@@ -14,37 +14,8 @@ export class PersonSpec {
 }
 
 export const testMappers: TestMapper[] = [];
-const testMapper1 = new TestMapper(`{ name: 'Greenpeace', volunteers: 3000 } / Employer`,
-    'EmployerSpec',
-    { name: 'Greenpeace', volunteers: 3000 },
-);
-testMappers.push(testMapper1);
-
-
-const testMapper2 = new TestMapper(`{ name: 'Total', employees: 30000 } / Employer`,
-    'EmployerSpec',
-    { name: 'Total', employees: 30000 },
-);
-testMappers.push(testMapper2);
-
-
-const testMapper3 = new TestMapper(`{ name: 'Total', employees: 30000 } / EmployerSpec`,
-    'EmployerSpec',
-    [{ name: 'Total', employees: 30000 }],
-    { shouldFail: true }
-);
-testMappers.push(testMapper3);
-
-
-const testMapper4 = new TestMapper(`[{ name: 'Total', volunteers: 3000 }] / EmployerSpec[]`,
-    'EmployerSpec[]',
-    [{ name: 'Total', volunteers: 3000 }],
-);
-testMappers.push(testMapper4);
-
-
-const testMapper5 = new TestMapper(`{ employer: { name: 'Total', employees: 30000 } } / PersonSpec`,
-    PersonSpec,
-    { employer: { name: 'Total', employees: 30000 } },
-);
-testMappers.push(testMapper5);
+testMappers.push(new TestMapper(`{ name: 'Greenpeace', volunteers: 3000 } / Employer`, 'EmployerSpec',{ name: 'Greenpeace', volunteers: 3000 }));
+testMappers.push(new TestMapper(`{ name: 'Total', employees: 30000 } / Employer`, 'EmployerSpec',{ name: 'Total', employees: 30000 }));
+testMappers.push(new TestMapper(`{ name: 'Total', employees: 30000 } / EmployerSpec`, 'EmployerSpec',[{ name: 'Total', employees: 30000 }], { expectedValue: undefined }));
+testMappers.push(new TestMapper(`[{ name: 'Total', volunteers: 3000 }] / EmployerSpec[]`, 'EmployerSpec[]',[{ name: 'Total', volunteers: 3000 }]));
+testMappers.push(new TestMapper(`{ employer: { name: 'Total', employees: 30000 } } / PersonSpec`, PersonSpec,{ employer: { name: 'Total', employees: 30000 } }));
