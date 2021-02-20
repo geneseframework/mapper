a722d8ed36dd8d6b2c1e67e0379ff5bdb750608e
Gilles Fabre <gillesfabre34@gmail.com>
2021-02-20 11:30:02 +0100

    Added Object

--- src/debug/project/src/tests/objects.spec.ts
+++ src/debug/project/src/tests/objects.spec.ts
@@ -2,5 +2,7 @@ import { TestMapper } from '../../../../test-engine/test-mapper.model';
 
 export const testMappers: TestMapper[] = [];
 testMappers.push(new TestMapper(`{color: 'blue'} / object`, 'object',{color: 'blue'}, { isolate: true}));
+testMappers.push(new TestMapper(`{color: 'blue'} / object`, 'Object',{color: 'blue'}, { isolate: true}));
+testMappers.push(new TestMapper(`{color: 'blue'} / object`, Object,{color: 'blue'}, { isolate: true}));
 
 
--- src/utils/objects.util.ts
+++ src/utils/objects.util.ts
@@ -1,4 +1,5 @@
 import { MapTarget } from '../types/map-target.type';
+import * as chalk from 'chalk';
 
 
 export function isObjectOrObjectsArrayTarget(mapTarget: MapTarget<any>): boolean {
@@ -7,7 +8,7 @@ export function isObjectOrObjectsArrayTarget(mapTarget: MapTarget<any>): boolean
 
 
 export function isObjectTarget(mapTarget: MapTarget<any>): boolean {
-    if (mapTarget === {}) {
+    if (mapTarget === {} || mapTarget === Object) {
         return true;
     } else {
         return typeof mapTarget === 'string' && ['object', 'Object'].includes(mapTarget);
