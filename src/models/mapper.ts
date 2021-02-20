6369fc6e69e33e16a7bcd159f72c901b8dc696af
Gilles Fabre <gillesfabre34@gmail.com>
2021-02-20 10:23:50 +0100

    Cleaned code

--- src/models/mapper.ts
+++ src/models/mapper.ts
@@ -9,7 +9,6 @@ import { GLOBAL } from '../const/global.const';
 import { MapTarget } from '../types/map-target.type';
 import { ArrayOfPrimitiveElements, PrimitiveElement, PrimitiveType } from '../types/primitives.type';
 import { MapTypeService } from '../services/map-type.service';
-import { clone } from '../utils/clone.util';
 import { MapEnumService } from '../services/map-enum.service';
 import { getDeclarationKind, getTypeDeclaration } from '../utils/ast-declaration.util';
 import { TypeDeclarationKind } from '../enums/type-declaration.kind';
@@ -22,7 +21,6 @@ import { MapDateService } from '../services/map-date.service';
 import { MapTargetInfo } from '../types/map-target-info.type';
 import { isDateOrDatesArrayType } from '../utils/dates.util';
 import { isArray } from '../utils/arrays.util';
-import * as chalk from 'chalk';
 
 export class Mapper<T> {
 
@@ -118,108 +116,4 @@ export class Mapper<T> {
         return isArray(mapTarget);
     }
 
-
-
-
-    /**
-     * When an object haves a field named 'gnIndexableType', that means that this object haves a model like this :
-     * {
-     *   [key: string]: {
-     *       country: string
-     *      }
-     *   } = {
-     *      gnIndexableType: {
-     *           country: ''
-     *      }
-     *  };
-     * For each key of gnIndexableType field, this method returns the corresponding mapped object with the target model
-     * For example, this method will receive an object like this :
-     *
-     * gnIndexableType: {
-     *      country: ''
-     * }
-     *
-     * and will return something like this :
-     *
-     * {
-     *     fr: {
-     *         country: 'France'
-     *     },
-     *     en: {
-     *         country: 'England'
-     *     }
-     * }
-     * Caution: param target should be defined
-     */
-    _mapIndexableType(target: any, source: any): any {
-        if (target === undefined || target.gnIndexableType === undefined) {
-            console.warn('Impossible to map indexable types with undefined target.');
-            return undefined;
-        }
-        if (source === undefined) {
-            return target;
-        }
-        if (source === null) {
-            return null;
-        }
-        return Array.isArray(target.gnIndexableType) && target.gnIndexableType.length > 0
-            ? this._mapIndexableTypeArray(target.gnIndexableType[0], source)
-            : Object.assign({}, this._mapIndexableTypeObject(target.gnIndexableType, source));
-    }
-
-
-    _mapIndexableTypeArray(target: any[], source: any): any {
-        // const mappedObject: any = {};
-        // for (const key of Object.keys(source)) {
-        //     const deepMapped = this._diveMap({[key]: [target]}, source);
-        //     Object.assign(mappedObject, {[key]: deepMapped[key]});
-        // }
-        // return mappedObject;
-    }
-
-
-
-    _mapIndexableTypeObject(target: any, source: any): any {
-        // const mappedObject: any = {};
-        // for (const key of Object.keys(source)) {
-        //     Object.assign(mappedObject, {[key]: this._diveMap(target, source[key])});
-        // }
-        // return mappedObject;
-    }
-
-
-    /**
-     * Map array of objects
-     */
-    _mapArray(target: any[], source: any[]): any[] {
-        if (source === null) {
-            return null;
-        }
-        if (source === undefined || !Array.isArray(source) || (Array.isArray(target) && !Array.isArray(source))) {
-            return target;
-        }
-        if (!Array.isArray(target) || target.length === 0) {
-            console.warn('Impossible to map array of objects with undefined or empty array');
-            return undefined;
-        }
-        const arrayOfObjects: any[] = [];
-        const model = clone(target[0]);
-        for (const element of source) {
-            let mappedElement: any;
-            if (Array.isArray(model) && Array.isArray(element)) {
-                mappedElement = this._mapArray(model, element);
-            } else if (Array.isArray(model) && !Array.isArray(element) && !!element) {
-                return target;
-            } else {
-                // mappedElement = this._diveMap(model, element);
-            }
-            arrayOfObjects.push(mappedElement);
-        }
-        return arrayOfObjects;
-    }
-
-}
-
-export interface IndexableType {
-    gnIndexableType: {[key: string]: any};
 }
