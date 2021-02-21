f49d5bcb8c66e865e5b295d000198deefc2bcc4a
Gilles Fabre <gillesfabre34@gmail.com>
2021-02-21 11:20:36 +0100

    Unstable : adding instance-generator

--- src/services/flag.service.ts
+++ src/services/flag.service.ts
@@ -39,8 +39,6 @@ export class FlagService {
         GLOBAL.project.createSourceFile(GLOBAL.instanceGeneratorPath, code, {overwrite: true});
         GLOBAL.project.addSourceFileAtPath(GLOBAL.instanceGeneratorPath);
         GLOBAL.instanceGeneratorSourceFile = GLOBAL.project.getSourceFile(GLOBAL.instanceGeneratorPath);
-        GLOBAL.instanceGeneratorSourceFile.fixMissingImports();
-        GLOBAL.instanceGeneratorSourceFile.saveSync();
         console.log(chalk.blueBright('AFTER SAVESYNCCCCC'), GLOBAL.instanceGeneratorSourceFile.getFilePath());
         await this.setGlobalGenerateInstance();
         console.log(chalk.blueBright('AFTERR SETTTTTT'));
@@ -51,24 +49,44 @@ export class FlagService {
         let code = `\nexport function generateInstance<T>(instanceGenerator: InstanceGenerator<T>): T {\n` +
             `${tab}let instance: any;\n` +
             `${tab}switch (instanceGenerator.id) {\n`;
-        code = `${code}${tabs(2)}default:\n` +
-            `${tabs(3)}console.log(chalk.yellow('WARNING: No instance found for instanceGenerator id = '), instanceGenerator?.id);\n` +
-            `${tabs(3)}instance = undefined;\n` +
-            `${tabs(2)}}\n` +
-            `${tab}return instance;\n` +
             `}\n`;
         console.log(chalk.yellowBright('CODEEEEEEE'), code);
         return code;
     }
 
+    // private static async generateInstanceGeneratorFile(): Promise<void> {
+    //     const switchStatement : SwitchStatement = GLOBAL.generateInstancesSourceFile.getFirstDescendantByKind(SyntaxKind.SwitchStatement);
+    //     switchStatement.removeClauses([0, switchStatement.getClauses().length]);
+    //     let switchCode = `switch (instanceGenerator.id) {\n`;
+    //     for (const instanceGenerator of GLOBAL.instanceGenerators) {
+    //         switchCode = `${switchCode}${tab}${this.switchClause(instanceGenerator)}`;
+    //     }
+    //     switchCode = `${switchCode}${tab}default:
+    //     console.log(chalk.yellow('WARNING: No instance found for instanceGenerator id = '), instanceGenerator?.id);
+    //     instance = undefined;
+    // }`;
+    //     switchStatement.replaceWithText(switchCode);
+    //     GLOBAL.generateInstancesSourceFile.fixMissingImports();
+    //     GLOBAL.generateInstancesSourceFile.saveSync();
+    //     GLOBAL.generateInstance = await require(GLOBAL.generateInstancesSourceFile.getFilePath()).generateInstance;
+    // }
+
+
 
     private static async setGlobalGenerateInstance(): Promise<void> {
         const switchStatement: SwitchStatement = GLOBAL.instanceGeneratorSourceFile.getFirstDescendantByKind(SyntaxKind.SwitchStatement);
         console.log(chalk.yellowBright('switchStatementtttt'), switchStatement.getText());
-        let switchCode: string = '';
+        let switchCode = `switch (instanceGenerator.id) {\n`;
         for (const instanceGenerator of GLOBAL.instanceGenerators) {
             switchCode = `${switchCode}${tabs(2)}${this.switchClause(instanceGenerator)}`;
         }
+        switchCode = `${switchCode}${tab}default:\n` +
+            `console.log(chalk.yellow('WARNING: No instance found for instanceGenerator id = '), instanceGenerator?.id);\n` +
+            `instance = undefined;\n` +
+            `}\n`;
+        switchStatement.replaceWithText(switchCode);
+        GLOBAL.instanceGeneratorSourceFile.fixMissingImports();
+        GLOBAL.instanceGeneratorSourceFile.saveSync();
         // console.log(chalk.greenBright('generateInstanceGeneratorFileeeeee'), GLOBAL.instanceGeneratorSourceFile.getFilePath());
         // GLOBAL.generateInstance = await require(GLOBAL.instanceGeneratorSourceFile.getFilePath())?.generateInstance;
         console.log(chalk.yellowBright('AAAAAHHHHHH !!!!!!!!!'), GLOBAL.generateInstance);
