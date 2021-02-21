e8670a923207eef4596376770e6525c61a7df398
Gilles Fabre <gillesfabre34@gmail.com>
2021-02-21 18:59:30 +0100

    Refactored instance-generator file

--- src/debug/project/start.ts
+++ src/debug/project/start.ts
@@ -2,6 +2,9 @@ import { Mapper } from '../../models/mapper';
 import * as chalk from 'chalk';
 import { Cat } from './src/models/cat.model';
 import { ARRAY_CAT_DATAS } from './src/data/array-cats.data';
+import { GLOBAL } from '../../const/global.const';
+
+GLOBAL.debug = true;
 
 async function start() {
     console.log(chalk.yellowBright('Starts application...'));
