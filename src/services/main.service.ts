6b0c9b57136eab30097d872d3a3ca3214436e0cb
Gilles Fabre <gillesfabre34@gmail.com>
2021-03-07 12:06:55 +0100

    Refacto algo for indexable types

--- src/services/main.service.ts
+++ src/services/main.service.ts
@@ -54,7 +54,7 @@ export class MainService {
 
     // TODO : enums
     private static async mapString<T>(target: string, data: any, options?: CreateOptions): Promise<T | T[] | Primitive | ArrayOfPrimitiveElements | Date | Date[] | object | object[]> {
-        // console.log(chalk.greenBright('STARTTTT'), target, data, options, isArrayType(target));
+        console.log(chalk.greenBright('STARTTTT'), target, data, options, isArrayType(target));
         await CheckTargetsService.start(target);
         if (isNullOrUndefined(data) || isAny(target)) {
             return data;
