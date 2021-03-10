4bf7580891016d90dfec9490aa08d6cb8e132fd9
Gilles Fabre <gillesfabre34@gmail.com>
2021-03-10 15:51:25 +0100

    Removed ts-morph from mapTypeService

--- src/services/map/map-type.service.ts
+++ src/services/map/map-type.service.ts
@@ -1,8 +1,5 @@
-import { TypeAliasDeclaration } from 'ts-morph';
-import { getTypeDeclaration } from '../../utils/ast/ast-declaration.util';
 import { CreateOptions } from '../../models/create-options.model';
 import { MainService } from '../main.service';
-import * as chalk from 'chalk';
 import { TypeInfo } from '../../models/declarations/type-info.model';
 import { GLOBAL } from '../../const/global.const';
 
@@ -10,7 +7,6 @@ export class MapTypeService {
 
 
     static async create<T>(target: string, data: any, options: CreateOptions): Promise<any> {
-        const typeDeclaration: TypeAliasDeclaration = getTypeDeclaration(target) as TypeAliasDeclaration;
         // console.log(chalk.blueBright('MAP TYPEEEEE'), typeDeclaration.getStructure());
         const typeInfo: TypeInfo = GLOBAL.getTypeInfo(target);
         return await MainService.mapToString(typeInfo.type, data, options);
