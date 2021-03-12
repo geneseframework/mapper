912e4fd5406cc86f62bbb57511ea95ee24807886
Gilles Fabre <gillesfabre34@gmail.com>
2021-03-12 12:45:59 +0100

    Tests runs with removing instance.generator.js

--- src/create/services/map/map-class.service.ts
+++ src/create/services/map/map-class.service.ts
@@ -17,14 +17,17 @@ export class MapClassService<T> {
 
     static async createInstance(target: string, data: any, options: CreateOptions): Promise<any> {
         const classInfo: ClassInfo = GLOBAL.getClassInfo(target);
-        // console.log(chalk.cyanBright('CREATE INSTTTTT'), target, data, classInfo);
+        console.log(chalk.cyanBright('CREATE INSTTTTT'), target, data, classInfo);
+        // console.log(chalk.cyanBright('GLOBAL infos'), target, data, classInfo);
         if (classInfo.isAbstract) {
             throwWarning(`"${target}" is abstract and can't be instantiated.`);
             return undefined;
         }
         const instanceGenerator = new InstanceGenerator(target, classInfo.filePath, classInfo.numberOfConstructorArguments);
         // const instance: object = await generateInstance(instanceGenerator) as object;
+        console.log(chalk.cyanBright('CREATE INSTTTTT instanceGenerator'), instanceGenerator);
         const instance: object = await GLOBAL.generateInstance(instanceGenerator) as object;
+        console.log(chalk.cyanBright('CREATE INSTTTTT instance ????'), instance);
         await MapInstanceOrInterfaceService.map(data, options, instance, classInfo);
         return instance;
     }
