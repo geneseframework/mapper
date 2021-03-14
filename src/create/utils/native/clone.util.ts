9dfe2fdf1b36218335354833b153bacbfad9558f
Genese <63037808+geneseframework@users.noreply.github.com>
2021-03-14 08:15:40 +0100

    Compiles in Angular application (#74)
    * Refacto algo
    
    * Fixed indexable types
    
    * bugs
    
    * Removed link to INIT in global.init
    
    * Removed link to INIT in global.init
    
    * Modified .npmignore
    
    * Fixed bug in clone()
    
    * Compiles in Angular application
    
    Co-authored-by: Gilles Fabre <gillesfabre34@gmail.com>

--- src/create/utils/native/clone.util.ts
+++ src/create/utils/native/clone.util.ts
@@ -7,11 +7,11 @@ export function clone(model: any): any {
     if (model) {
         if (Array.isArray(model)) {
             const newArray = [];
-            model.forEach(item => newArray.push(this.clone(item)));
+            model.forEach(item => newArray.push(clone(item)));
             return newArray;
         } else if (typeof model === 'object') {
             const newObject = {};
-            Object.keys(model).forEach(key => newObject[key] = this.clone(model[key]));
+            Object.keys(model).forEach(key => newObject[key] = clone(model[key]));
             return newObject;
         } else {
             return model;
