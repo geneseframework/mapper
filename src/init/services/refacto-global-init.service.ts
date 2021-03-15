77a3a97b053f568209fe29b244d1df723e89f519
Gilles Fabre <gillesfabre34@gmail.com>
2021-03-15 19:20:28 +0100

    Fixed bug with path in global-init.service TAG 210315-1919

--- src/init/services/refacto-global-init.service.ts
+++ src/init/services/refacto-global-init.service.ts
@@ -1,17 +1,14 @@
 import { ImportDeclaration, SourceFile } from 'ts-morph';
 import { INIT } from '../const/init.const';
-import * as chalk from 'chalk';
 
 export class RefactoGlobalInitService {
 
+
     static async init(): Promise<void> {
-        const globalInitSourceFile: SourceFile = INIT.project.getSourceFile(INIT.globalInitPath);
-        console.log(chalk.magentaBright('GLOB PATHHHHHH'), globalInitSourceFile.getBaseName());
-        console.log(chalk.magentaBright('GLOB INIT.declarationInfoPath'), INIT.declarationInfoPath);
-        console.log(chalk.magentaBright('GLOB INIT.instanceGeneratorPath'), INIT.instanceGeneratorPath);
+        const globalInitSourceFile: SourceFile = INIT.project.addSourceFileAtPath(INIT.globalInitPath);
         this.getImportDeclaration('declarationInfos', globalInitSourceFile).setModuleSpecifier(INIT.declarationInfoPath);
         this.getImportDeclaration('generateInstance', globalInitSourceFile).setModuleSpecifier(INIT.instanceGeneratorPath);
-        // globalInitSourceFile.saveSync();
+        globalInitSourceFile.saveSync();
     }
 
 
