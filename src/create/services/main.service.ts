410e05ce7909470913710ce33b42d749a1bbb8b2
Gilles Fabre <gillesfabre34@gmail.com>
2021-03-20 12:51:12 +0100

    Removed async from main.service

--- src/create/services/main.service.ts
+++ src/create/services/main.service.ts
@@ -43,22 +43,22 @@ export class MainService {
      * @param data
      * @param options
      */
-    static async map<T>(target: Target<T>, data: any, options?: MapperConfig): Promise<T | T[] | Primitive | ArrayOfPrimitiveElements | Date | Date[] | object | object[]> {
+    static map<T>(target: Target<T>, data: any, options?: MapperConfig): T | T[] | Primitive | ArrayOfPrimitiveElements | Date | Date[] | object | object[] {
         GLOBAL.start = Date.now();
-        await GlobalInitService.start();
+         GlobalInitService.start();
         if (!OptionsService.wasInitialized(options)) {
             options = OptionsService.initialize(options);
         }
-        return await this.mapToString<T>(target, data, options);
+        return this.mapToString<T>(target, data, options);
     }
 
 
-    static async mapToString<T>(target: Target<T>, data: any, options?: MapperConfig): Promise<T | T[] | Primitive | ArrayOfPrimitiveElements | Date | Date[] | object | object[]> {
-        return await this.mapString(TargetService.toString(target), data, options);
+    static mapToString<T>(target: Target<T>, data: any, options?: MapperConfig): T | T[] | Primitive | ArrayOfPrimitiveElements | Date | Date[] | object | object[] {
+        return this.mapString(TargetService.toString(target), data, options);
     }
 
 
-    private static async mapString<T>(target: string, data: any, options?: MapperConfig): Promise<T | T[] | Primitive | ArrayOfPrimitiveElements | Date | Date[] | object | object[]> {
+    private static mapString<T>(target: string, data: any, options?: MapperConfig): T | T[] | Primitive | ArrayOfPrimitiveElements | Date | Date[] | object | object[] {
         // console.log(chalk.greenBright('MAP STRRRRR'), target, data);
         CheckTargetsService.start(target);
         if (isNullOrUndefined(data) || isAny(target)) {
@@ -68,11 +68,11 @@ export class MainService {
         } else if (isStringAsNullOrLiteral(target)) {
             return MapNullOrLiteralService.create(target);
         } else if (isBracketed(target)) {
-            return await MapTupleService.create(target, data, options)
+            return MapTupleService.create(target, data, options)
         } else if (isArrayType(target)) {
-            return await MapArrayService.create(target, data, options);
+            return MapArrayService.create(target, data, options);
         } else if (hasGeneric(target)) {
-            return await MapGenericService.create(target, data, options);
+            return MapGenericService.create(target, data, options);
         } else if (isPrimitiveTypeName(target)) {
             return MapPrimitiveService.create(target, data, options);
         } else if (isQuoted(target)) {
@@ -82,15 +82,15 @@ export class MainService {
         } else if (isCurveBracketed(target)) {
             return MapObjectService.create(target, data, options)
         } else if (hasDeclaration(target)) {
-            return await MapDeclarationService.create(target, data, options);
+            return MapDeclarationService.create(target, data, options);
         } else if (hasSeparators(target)) {
-            return await MapComplexService.create(target, data, options);
-        // } else if (await isDeclaredOutOfProjectAddItToGlobal(target)) { // TODO
-        //     return await MapOutOfProjectService.create(target, data, options);
+            return MapComplexService.create(target, data, options);
+        // } else if ( isDeclaredOutOfProjectAddItToGlobal(target)) { // TODO
+        //     return MapOutOfProjectService.create(target, data, options);
         } else {
             // TODO : finish to implement
             throwWarning(`type not found : "${target}".`);
-            return await MapComplexService.create(target, data, options);
+            return MapComplexService.create(target, data, options);
         }
     }
 
