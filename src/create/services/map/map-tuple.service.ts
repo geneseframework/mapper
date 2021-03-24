6c0f0df1a4d30fe107cc88e0cb64cc81bba23e84
Gilles Fabre <gillesfabre34@gmail.com>
2021-03-24 11:15:54 +0100

    Refacto MapTuple

--- src/create/services/map/map-tuple.service.ts
+++ src/create/services/map/map-tuple.service.ts
@@ -7,10 +7,24 @@ import { MapperBehavior } from '../../../shared/models/config-behavior.model';
 export class MapTupleService<T> {
 
 
+    /**
+     * Returns mapped tuple if data is correct, undefined if not
+     * @param target    // The target with tuple format
+     * @param data      // The data to map
+     * @param options   // The create() options
+     */
     static create(target: Bracketed, data: any, options: MapperBehavior): any[] {
-        if (!isArrayOfSameLength(target, data)) {
-            return undefined;
-        }
+        return isArrayOfSameLength(target, data) ? this.mapTuple(target, data, options) : undefined;
+    }
+
+
+    /**
+     * Returns mapped tuple
+     * @param target    // The target with tuple format
+     * @param data      // The data to map
+     * @param options   // The create() options
+     */
+    private static mapTuple(target: Bracketed, data: any, options: MapperBehavior) {
         const tuple: any[] = [];
         for (let i = 0; i < data.length; i++) {
             if (isNullOrUndefined(data[i])) {
