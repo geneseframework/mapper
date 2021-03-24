0a2ba4aa60627c1ad7768ffabe3ccd2f6d78f08d
Gilles Fabre <gillesfabre34@gmail.com>
2021-03-24 12:34:01 +0100

    Cleaned code

--- src/create/services/map/map-object-type.service.ts
+++ src/create/services/map/map-object-type.service.ts
@@ -1,36 +1,13 @@
-import { isArray } from '../../utils/native/arrays.util';
-import { isObject, isObjectWhichIsNotArray } from '../../utils/native/objects.util';
+import { isObject } from '../../utils/native/objects.util';
 
 export class MapObjectTypeService {
 
-
     /**
-     * If target is 'object' or 'Object', this method returns mapped data if data is an object or an array
-     * @param data
+     * If target is 'object' or 'Object', this method returns mapped data if data is an object or an array, undefined if not
+     * @param data  // The data to map
      */
     static create(data: object): object {
         return isObject(data) ? data : undefined;
-        // return isArray(data) ? this.createArrayObjects(data as object[]) : this.createObject(data);
-    }
-
-
-    private static createArrayObjects(data: object[]): object[] {
-        const objectsArray = [];
-        for (const element of data) {
-            const objectElement: object = this.createObject(element);
-            if (objectElement || element === undefined || element === null) {
-                objectsArray.push(objectElement);
-            }
-        }
-        return objectsArray;
-    }
-
-
-    private static createObject(data: object): object {
-        if (data === null) {
-            return null;
-        }
-        return isObjectWhichIsNotArray(data) ? data : undefined;
     }
 
 }
