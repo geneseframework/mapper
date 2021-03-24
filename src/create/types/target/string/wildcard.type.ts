35c5904aacdf9c160d5ba6646b94f9f1c16c89a8
Gilles Fabre <gillesfabre34@gmail.com>
2021-03-24 11:52:30 +0100

    Refactored MapArrayService

--- src/create/types/target/string/wildcard.type.ts
+++ src/create/types/target/string/wildcard.type.ts
@@ -1,6 +1,6 @@
 export type WildCard = 'any' | 'unknown' | undefined;
 
 
-export function isWildCard(typeName: string): boolean {
+export function isWildCard(typeName: string): typeName is WildCard {
     return typeName === 'any' || typeName === undefined || typeName === 'unknown';
 }
