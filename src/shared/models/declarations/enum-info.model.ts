8716ba96e77e95b6bce337930385217f3386c7da
Gilles Fabre <gillesfabre34@gmail.com>
2021-04-01 16:37:56 +0200

    Added comments

--- src/shared/models/declarations/enum-info.model.ts
+++ src/shared/models/declarations/enum-info.model.ts
@@ -1,8 +1,11 @@
 import { DeclarationInfo } from './declaration-info.model';
-
+/**
+ * Info about enums declared in the user's project.
+ * These infos will be added in the generated file declaration-infos.js and then used at runtime by the create() method
+ */
 export class EnumInfo extends DeclarationInfo {
 
-    initializers: any[] = [];
+    initializers: any[] = [];       // The values of the enum members
 
     constructor(name: string, filePath: string, typeParameters: any[] = []) {
         super(name, filePath, 'Enum', typeParameters);
